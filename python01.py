26e48a314155a5ae7301646774e7cd8ecffc04b8
Daniel <Daniel@danieldeMacBook-Air.local>
2017-05-22 23:57:16 +0800

    fisrt 0522 python3

---
+++ python01.py
@@ -0,0 +1,7 @@
+print("hello world")
+
+age=20
+name='Swaroop'
+print('{0}was{1} years old when he wrote this book'.format(name,age))
+print('why is {0} playing with that python?'.format(name))
+print(name+"is"+str(age)+"years old")

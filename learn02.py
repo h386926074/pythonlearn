7ebc75207c6ec061bf085cadd60241edff58bd2f
huangfeng <huangfeng@163.com>
2017-05-25 08:41:06 +0800

    function 参数带数组字典

--- learn02.py
+++ learn02.py
@@ -1,4 +1,33 @@
+# -*- coding: UTF-8 -*-
 print('hello world')
 age = 1
 name= 'haungfeng'
 print("my name is {}, my age is {}".format(name,age))
+
+#hello world
+
+myname = '''这是一个多行字符串，这是它的第一行
+        THIS IS THE SECOND LINE.
+        '''
+print(myname +' is ' + name)
+
+print('{0:.3f}'.format(1.0/3))  #保留小数后几位
+print('{0:_^11}'.format('hello'))
+print('{name} wrote {book}'.format(name='huangfeng',book='a byte of python'))
+
+#print('a',end= '')
+#print('b',end= '')
+
+print(r'what\'s \t my')
+
+number = 23
+guess = int(input('enter an integer: '))
+if guess==number:
+    print('congratulations,you guessed it')
+elif guess < number:
+    print('No,it is a litter higher than that.')
+else:
+    print('No.it is a litter lower than that')
+
+
+    
---
+++ learn03.py
@@ -0,0 +1,25 @@
+for i in range(1,5):
+    print(i)
+else:
+    print('the for loop is over')
+
+x= 50
+
+def func():
+    global x
+    print('x is',x)
+    x = 2
+    print('changed golbal x to',x)
+
+func()
+print('value of x is',x)
+
+def total(a=5,*numbers,**phonebook):
+    print('a',a)
+    for single_item in numbers:
+        print('single_item',single_item)
+
+    for first_part,second_part in phonebook.items():
+        print(first_part,second_part)
+
+print(total(110,1,2,3,jack=1123))

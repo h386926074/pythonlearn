dc415a83e3ea62b917f0ab0deb4559d3394bc293
huangfeng <huangfeng@163.com>
2017-05-27 08:56:21 +0800

    Robot

---
+++ class01.py
@@ -0,0 +1,7 @@
+# -*- coding:utf8 -*-
+class Person:
+    def say_hi(self):
+        print('Hello world ,how are you!')
+
+p = Person()
+p.say_hi()
---
+++ oop_objvar.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8
+
+class Robot:
+    '''表示有一个带有名字的机器人'''
+
+    # 一个类变量
+    population=0
+
+
+    def __init__(self,name):
+        '''初始化数据'''
+        self.name = name
+        print("(Initializing {})".format(self.name))
+
+        # 当有人被创建时，机器人将会增加人口数量
+        Robot.population +=1
+
+
+    

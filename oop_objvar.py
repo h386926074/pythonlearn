178306c665295856f8cab623aebf29b8e4d8bced
huangfeng <huangfeng@163.com>
2017-05-27 10:02:34 +0800

    12

--- oop_objvar.py
+++ oop_objvar.py
@@ -1,9 +1,10 @@
-# -*- coding:utf-8
+# coding=UTF-8
 
 class Robot:
     '''表示有一个带有名字的机器人'''
 
     # 一个类变量
+
     population=0
 
 
@@ -15,5 +16,9 @@ class Robot:
         # 当有人被创建时，机器人将会增加人口数量
         Robot.population +=1
 
+    @classmethod
+    def say_hi(self):
+        print('hello world')
 
-    
+
+Robot.say_hi()
